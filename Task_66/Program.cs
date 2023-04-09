49cabe50fc05f72e9b770efcc2f3202fc73c6c0e
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-04-09 05:37:04 +0300

    Задача 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,43 @@
+﻿int m = UserInput("Введите целое число  M: ", "Ошибка!");
+int n = UserInput("Введите целое число N: ", "Ошибка!");
+
+int sum = SumNumbers(m, n);
+Console.WriteLine($"M = {m}; N = {n} -> {sum}");
+
+
+//============================Методы=====================
+
+//-----------Ввод данных
+int UserInput(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if ((int.TryParse(Console.ReadLine(), out int userNumber)) && userNumber > 0)
+
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+//-------------Вывод суммы от M до N рекурсией
+int SumNumbers(int m, int n)
+{
+    int sum = 0;
+    if (m < n)
+    {
+        if (m == n)
+            return sum + n;
+
+        return m + SumNumbers(m + 1, n);
+
+    }
+    else                                        // На случай, если пользователь введет М > N
+    {
+        if (m == n)
+            return sum + n;
+
+        return m + SumNumbers(m - 1, n);
+    }
+}
